a0b2f789995ff2cb8e222e6e0a57d00189df4a4d
Kuman13 <kyym13@gmail.com>
2023-09-29 07:37:03 +0200

    added_second_task

--- hw3/script.js
+++ hw3/script.js
@@ -1,15 +1,29 @@
-let fingerNumber = prompt("Enter the finger number on your hand (from 1 to 5):");
+// let fingerNumber = prompt("Enter the finger number on your hand (from 1 to 5):");
 
-if (fingerNumber === '1') {
-    alert("Thumb");
-} else if (fingerNumber === '2') {
-    alert("Index finger");
-} else if (fingerNumber === '3') {
-    alert("Middle finger");
-} else if (fingerNumber === '4') {
-    alert("Ring finger");
-} else if (fingerNumber === '5') {
-    alert("Pinky");
+// if (fingerNumber === '1') {
+//     alert("Thumb");
+// } else if (fingerNumber === '2') {
+//     alert("Index finger");
+// } else if (fingerNumber === '3') {
+//     alert("Middle finger");
+// } else if (fingerNumber === '4') {
+//     alert("Ring finger");
+// } else if (fingerNumber === '5') {
+//     alert("Pinky");
+// } else {
+//     alert("Invalid finger number. Please enter a number from 1 to 5.");
+// }
+
+
+let canFly = prompt('This animal can fly? (Yes/No)');
+
+if (canFly === 'yes') {
+    alert ('this is bird!');
 } else {
-    alert("Invalid finger number. Please enter a number from 1 to 5.");
+    let livesInWater = prompt("This is animal lives in water? (yes/no)");
+    if (livesInWater === "yes") {
+        alert("this is fish!");
+    } else {
+        alert("this mammal");
+    }
 }
