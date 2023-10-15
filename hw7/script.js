bf89ce5222f2cf2355d7f83a0c5193969c3fda9a
Kuman13 <kyym13@gmail.com>
2023-10-15 17:05:10 +0200

    added_hw7

---
+++ hw7/script.js
@@ -0,0 +1,16 @@
+function removeElement(array, item, newEl) {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === item) {
+        array[i] = newEl;
+      }
+    }
+    return array;
+  }
+
+  const myArray = [1, 2, 3, 4, 5,];
+  const itemToRemove = 3;
+  const newItem = 10;
+  
+  const newArray = removeElement(myArray, itemToRemove, newItem);
+  console.log(newArray);
+  
